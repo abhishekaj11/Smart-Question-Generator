3c19b78011723cf01bee5456cdecb5f8c3525d7a
abhishekaj11 <prithivaj11@gmail.com>
2025-10-06 19:05:53 +0530

    Generate Qns Logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
-from database.connection import get_database 
+import json
+from backend.database.connection import get_database 
+from backend.generate_questions import generate_question_paper
 
 app = Flask(__name__)
-
+"""
 @app.route("/", methods=["GET", "POST"])
 def register():
     collection = get_database("SQG")["Login"]
@@ -25,12 +27,22 @@ def register():
         return redirect(url_for("home", user=email))  # 👈 pass email
 
     return render_template("index.html")
-
+"""
 
 @app.route("/home")
 def home():
     user = request.args.get("user")  # 👈 fetch email from URL
     return render_template("home.html", user=user)
+
+
+@app.route("/generate", methods=["GET"])
+def generate():
+    coursecode = request.form.get("subcode")
+    collection = get_database("SQG")["syllabusC"]
+    syllabus = collection.find_one({"courseCode": coursecode})
+    paper = generate_question_paper(syllabus)
+    return jsonify({"question_paper": paper})
+
 
 
 if __name__ == "__main__":
